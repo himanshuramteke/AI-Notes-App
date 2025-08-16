26f3c1cfa81ac9a95eba543ae4e075bd0e354350
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-08-16 16:07:44 +0530

    Added HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
-const Homepage = () => {
-  return <div>Homepage</div>;
+import { getUser } from "@/auth/server";
+import AskAIButton from "@/components/AskAIButton";
+import NewNoteButton from "@/components/NewNoteButton";
+import NoteTextInput from "@/components/NoteTextInput";
+import { prisma } from "@/db/primsa";
+
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+const Homepage = async ({ searchParams }: Props) => {
+  const noteIdParam = (await searchParams).noteId;
+  const user = await getUser();
+
+  const noteId = Array.isArray(noteIdParam)
+    ? noteIdParam![0]
+    : noteIdParam || "";
+
+  const note = await prisma.note.findUnique({
+    where: { id: noteId, authorId: user?.id },
+  });
+
+  return (
+    <div className="flex flex-col h-full items-center gap-4">
+      <div className="flex w-full max-w-4xl justify-end gap-2">
+        <AskAIButton user={user} />
+        <NewNoteButton user={user} />
+      </div>
+
+      <NoteTextInput noteId={noteId} startingNodeText={note?.text || ""} />
+    </div>
+  );
 };
 
 export default Homepage;
---
+++ src/components/NoteTextInput.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+type Props = {
+  noteId: string;
+  startingNodeText: string;
+};
+
+function NoteTextInput({ noteId, startingNodeText }: Props) {
+  return <div>NoteTextInput</div>;
+}
+
+export default NoteTextInput;
